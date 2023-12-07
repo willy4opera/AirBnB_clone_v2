7c12f4a18e56c759fe8bb193987967f234b893fa
odionye Obiajulu Williams <willy4opera@gmail.com>
2023-12-07 12:04:23 -0500

    added the 1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Fabfile: Automates the generation of .tgz
+# archive from the contents of web_static.
+
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """ Here, we created the tar gzipped archive of the directory web_static."""
+    date_t = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         date_t.month,
+                                                         date_t.day,
+                                                         date_t.hour,
+                                                         date_t.minute,
+                                                         date_t.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
+
