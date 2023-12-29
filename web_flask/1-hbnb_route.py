a3deacb0972acbfff0610e61e65d2d86d1d03094
willyopera <willy4opera@gmail.com>
2023-12-29 08:50:46 -0500

    added the 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Here, we started the Flask web application.
+
+The application listens at 0.0.0.0:5000.
+Routes:
+    /: Show 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
