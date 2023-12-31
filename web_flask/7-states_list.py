06814ae95ff57af3c88847e2376548debd014313
willyopera <willy4opera@gmail.com>
2023-12-31 06:44:38 -0500

    Debugged 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,29 +6,26 @@ The application listens at 0.0.0.0:5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
-
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
-
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
+    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Show the HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
+    """list states sorted by name
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
