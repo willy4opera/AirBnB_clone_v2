fbdbad656d85f66ab5810341d8763ed4bf027ab0
willyopera <willy4opera@gmail.com>
2024-01-01 08:27:34 -0500

    Debugged 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
+"""simple flask app
 """
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
-
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
     storage.close()
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def states_cities_list():
+    """list states and cities sorted by name
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
